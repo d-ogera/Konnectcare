cc7b4b5fc07dd2ce3239b04185c07a73759c2772
Ogera <107644200+d-ogera@users.noreply.github.com>
2024-09-24 21:16:23 +0300

    Update head_start.php

--- static/head_start.php
+++ static/head_start.php
@@ -1,1 +1,15 @@
-<?php	$webname = "KonnectCare";	$webtag = "Quality healthcare at your comfort";	$weburl = "https://care.medcrine.com";	$webphone = "+254790173296";	$webemail = "care@medcrine.com";?><!doctype html><html lang="en">  <head>    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    <meta http-equiv="x-ua-compatible" content="ie=edge">    <title><?php echo $webname;?> - <?php echo $webtag;?></title>    <link rel="shortcut icon" href="<?php echo $weburl;?>/assets/images/site_logo/favicon.png">
+<?php
+	$webname = "KonnectCare";
+	$webtag = "Quality healthcare at your comfort";
+	$weburl = "https://care.medcrine.com";
+	$webphone = "+254790173296";
+	$webemail = "care@medcrine.com";
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php echo $webname;?> - <?php echo $webtag;?></title>
+    <link rel="shortcut icon" href="<?php echo $weburl;?>/assets/images/site_logo/favicon.png">
